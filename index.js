15af02495da9db1bee4f12b53bdd01aaae40a1b2
Jason-91 <hyunjoon1991@gmail.com>
2022-10-17 16:38:42 -0700

    improved functionality search-marketboard endpoint tested and debugged several variables and url cleaned up code commented future potential reorganizing

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 const app = express();
 const port = 8000;
 
+// All key should exist in .env file
 const xivApiPrivateKey = '77e71ea12ea143c7b0520295eb1cc31b0be74519e5924792917a4c191ae44fbc';
 
 app.use(cors());
@@ -18,10 +19,11 @@ app.get('/search-itemname', async (req, res) => {
         baseURL: `https://xivapi.com`
     })
     .then((value) => {
+        // console.log('This is the value:', value);
         const { data } = value;
         const { Results } = data;
         // const Results = value.data.Results
-        // console.log(Results);
+        // console.log('These are the Results:', Results);
         result = {
             message: 'success',
             data: Results.map((data) => {
@@ -40,25 +42,48 @@ app.get('/search-itemname', async (req, res) => {
     res.send(result);
 });
 
+// if not being used, remove
 const dataCenter = 'Crystal'
 // const world = 'Malboro'
 // const allWorlds = ['Crystal', 'Balmung', 'Bryhildr', 'Coeurl', 'Diabolos', 'Goblin', 'Mateus', 'Malboro', 'Zalera']
 
 app.get('/search-marketboard', async (req, res) => {
-    const data_ID = req.query.dataID;
+    // console.log('Queries:', req.query)
+    const dataID = req.query['data-id'];
+    // console.log(dataID);
+    const dataCenter = req.query.dataCenter;
+    // console.log(dataCenter);
     let result;
+    let listingsResultsData;
+    let listingsResults;
+    let historyResultsData;
+    let historyResults;
 
     await axios ({
         method: 'get',
-        url: `/api/v2/${dataCenter}/${data_ID}`,
+        url: `/api/v2/${dataCenter}/${dataID}?listings=100`,
         baseURL: `https://universalis.app`
     })
     .then(async (value) => {
+        // console.log('Listings value:', value);
         const { data } = value;
-        const { listings } = data;
+        // console.log('Listings value.data:', data);
+        const { listings, averagePriceNQ, averagePriceHQ, currentAveragePriceNQ, currentAveragePriceHQ, nqSaleVelocity, hqSaleVelocity, minPriceNQ, maxPriceNQ, minPriceHQ, maxPriceHQ } = data;
         // listings = value.data.listings
+        // averagePriceNQ = value.data.averagePriceNQ
         // console.log(listings);
-        let listingsResults = [];
+        listingsResultsData = {
+            averagePriceNQ,
+            averagePriceHQ,
+            currentAveragePriceNQ,
+            currentAveragePriceHQ,
+            nqSaleVelocity,
+            hqSaleVelocity,
+            minPriceNQ,
+            maxPriceNQ,
+            minPriceHQ,
+            maxPriceHQ,
+        };
         listingsResults = listings.map(element => {
             return {
                 listings_lastReviewTime: element.lastReviewTime,
@@ -68,21 +93,27 @@ app.get('/search-marketboard', async (req, res) => {
                 listings_hq: element.hq,
                 listings_retainerName: element.retainerName,
                 listings_total: element.total,
-            }
+            };
         })
 
         await axios({
             method: 'get',
-            url: `api/v2/history/${dataCenter}/${data_ID}?entriesToReturn=100`,
+            url: `api/v2/history/${dataCenter}/${dataID}?entriesToReturn=100`,
             baseURL: `https://universalis.app`
         })
         .then((value) => {
+            // console.log('History value:', value);
             const { data } = value;
-            const { entries } = data;
+            // console.log('History value.data:', data);
+            const { entries, nqSaleVelocity, hqSaleVelocity } = data;
             // entries = value.data.entries
+            // nqSaleVelocity = value.data.nqSaleVelocity
             // console.log(entries);
-            let entriesResults = [];
-            entriesResults = entries.map(element => {
+            historyResultsData = {
+                nqSaleVelocity,
+                hqSaleVelocity,
+            };
+            historyResults = entries.map(element => {
                 return {
                     entries_hq: element.hq,
                     entries_pricePerUnit: element.pricePerUnit,
@@ -97,7 +128,11 @@ app.get('/search-marketboard', async (req, res) => {
     .catch((error) => {
         result = { message: 'error: critical failure, critical miss, or fumble' }
     });
-
+    // indentation
+    result = {listingsResultsData,
+    listingsResults,
+    historyResultsData,
+    historyResults,}
     res.send(result);
 });
 
